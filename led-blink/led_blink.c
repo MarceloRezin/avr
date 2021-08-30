b8176701c084fb40adc30c1fea0dc421b124e4eb
Marcelo Rezin <marcelo.rezin@gmail.com>
2021-08-30 20:02:21 -0300

    Pisca o led utilizando uma função auxiliar

--- led-blink/led_blink.c
+++ led-blink/led_blink.c
@@ -1,12 +1,20 @@
 // Device: attiny2313a
 // Eu quero pb4
 
+#define F_CPU	1000000UL //Frequencia do mcu
+
 #include <avr/io.h>
+#include <util/delay.h>
 
 int main() {
 
 	DDRB	= 0b00010000;
-	PORTB	= 0b00010000;
+	while(1) {
+		PORTB	= 0b00010000;
 
-	return 0;
+		_delay_ms(500);
+		PORTB	= 0b00000000;
+
+		_delay_ms(500);
+	}
 }
