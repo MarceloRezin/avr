08897cf6b58d9a1a675aa17be2761ec1f40197d7
Marcelo Rezin <marcelo.rezin@gmail.com>
2023-02-04 16:03:11 -0300

    Vetor de interrupção

---
+++ i2c_master/interrupt_vector.asm
@@ -0,0 +1,21 @@
+rjmp	SETUP	; RESET -> vai para o início do código
+reti			; INT0
+reti			; INT1
+reti			; TIMER1 CAPT
+reti			; TIMER1 COMPA
+reti			; TIMER1 OVF
+reti 			; TIMER0 OVF
+reti			; USART0, RX
+reti			; USART0, UDRE
+reti			; USART0, TX
+reti			; ANALOG COMP
+reti			; PCINT0
+reti			; TIMER1 COMPB
+rjmp	CAINT	; TIMER0 COMPA
+reti			; TIMER0 COMPB
+reti			; USI START
+reti			; USI OVERFLOW
+reti			; EE READY
+reti			; WDT OVERFLOW
+reti			; PCINT1
+reti			; PCINT2
