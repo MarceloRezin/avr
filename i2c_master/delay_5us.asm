99a01fb1664d970badb14410bf449245ca5e6d3c
Marcelo Rezin <marcelo.rezin@gmail.com>
2024-01-27 20:17:58 -0300

    Códigos não acabados da comunicação I2C com assembly

--- i2c_master/delay_5us.asm
+++ i2c_master/delay_5us.asm
@@ -1,47 +1,20 @@
 ; Rotina DELAY_5US, utilizado para gerar um delay de 5us, para o SCL do I2C.
 ; Baseado no clock do MCU em 8MHZ
 
+;1mhz 
+;1 cl = 1us
+
 DELAY_5US: ; 4.75 + 0.125 (do OUT)
 
 	;chamada: 0.375us
 
-	;1 us
-	nop
-	nop
-	nop
-	nop
-	nop
+	;rcall 3cl
+
+	;3 us
 	nop
 	nop
 	nop
 
-	;1 us
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-
-	;1 us
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-
-	;0.875 us
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
 
 	ret ; 0.5 us
+	;4 cl
--- i2c_master/i2c_master.asm
+++ i2c_master/i2c_master.asm
@@ -4,7 +4,7 @@
 ;.set SLAVE_ADDRESS_READ = 0b11010001
 .set SLAVE_ADDRESS_READ = 0b10101010
 ; .set SLAVE_ADDRESS_WRITE = 0b11010000
-.set SLAVE_ADDRESS_WRITE = 0b10010000
+.set SLAVE_ADDRESS_WRITE = 0b00000001
 
 
 
@@ -27,120 +27,238 @@ DATA_WRITE_1_BYTE:
 	ldi r18, SLAVE_ADDRESS_WRITE
 	ldi r19, (1<<USIWM1) | (1<<USITC) ; so inverte o SCL
 	ldi r20, (1<<USIWM1) | (1<<USICLK) ; Shift no SDA
-	ldi r21, (1<<USISIF)
+	ldi r21, (1<<USICNT2) | (1<<USICNT1) | (1<<USICNT0)
+	
+	out USISR, r21
 
 	; abaixa SDA e faz shift, como ta tudo zerado, não tem problema
 	out USICR, r20
 	rcall DELAY_5US
 	rcall DELAY_5US
 
-	out USICR, r19 ;MSB  _
-	rcall DELAY_5US
-	rcall DELAY_5US
-	out USIDR, r18 ;S
-	rcall DELAY_5US
-	rcall DELAY_5US
-	rcall DELAY_5US
-	rcall DELAY_5US
-	rcall DELAY_5US
-	rcall DELAY_5US
-	rcall DELAY_5US
-	rcall DELAY_5US
-	rcall DELAY_5US
-	rcall DELAY_5US
-	rcall DELAY_5US
-	rcall DELAY_5US
-	rcall DELAY_5US
-	rcall DELAY_5US
-	rcall DELAY_5US
-	rcall DELAY_5US
-	out USISR, r21 ; Limpa start condition
-	out USICR, r19 ; -
+	out USICR, r19 ; Abaixa SCL e gera a start condition
 	
+	rjmp WAIT_START_CONDITION
+
+
+WAIT_START_CONDITION:
+	in r22, USISR 
+	sbrs r22, USISIF
+	rjmp WAIT_START_CONDITION
+
+	ldi r21, (1<<USISIF)
+
+	or r22, r21
+	out USISR, r22 ; Limpa start condition
+
+	rjmp SET_DATA_SEND
+
+SET_DATA_SEND:
 	rcall DELAY_5US
 	rcall DELAY_5US
-	; cbi	PORTB, PINB7
+	
+	out USIDR, r18 ;libera informação do SDA
+	rjmp SEND_LOOP
+
+SEND_LOOP:
+	
+	out USICR, r19 ; -
+	rcall DELAY_5US
+	rcall DELAY_5US
 	out USICR, r19 ; _
 	rcall DELAY_5US
 	rcall DELAY_5US
-	out USICR, r20 ;
+	out USICR, r20 ; Shift
 	rcall DELAY_5US
 	rcall DELAY_5US 
+
+	in r22, USISR 
+	sbrs r22, USIOIF
+	rjmp SEND_LOOP
+	; end
+	; TODO: talvez da qui pra baixo em uma nova label
+
+	ldi r21, (1<<USIOIF)
+	out USISR, r21 ; 
+
+	; ACK 
+	; set sda como input
+	ldi r21, (1<<PINB7) | (0<<PINB5) ; todo mais facil
+	; in r21, DDRB
+	; cbr r21, PINB5
+	out DDRB, r21
+	
+	;puso no scl
 	out USICR, r19 ; -
-
 	rcall DELAY_5US
 	rcall DELAY_5US
 	out USICR, r19 ; _
 	rcall DELAY_5US
-	rcall DELAY_5US
-	out USICR, r20 ; 
-	rcall DELAY_5US
-	rcall DELAY_5US
-	out USICR, r19 ; -
+	rcall DELAY_5US 
 
-	rcall DELAY_5US
-	rcall DELAY_5US
-	out USICR, r19 ; _
-	rcall DELAY_5US
-	rcall DELAY_5US
-	out USICR, r20 ; 
-	rcall DELAY_5US
-	rcall DELAY_5US
-	out USICR, r19 ; -
+   ; set sda como output
+	ldi r21, (1<<PINB7) | (1<<PINB5) ; todo mais facil
+	; in r21, DDRB
+	; cbr r21, PINB5
+	out DDRB, r21
+	; ACK END 
 
-	rcall DELAY_5US
-	rcall DELAY_5US
-	out USICR, r19 ; _
-	rcall DELAY_5US
-	rcall DELAY_5US
-	out USICR, r20 ; 
-	rcall DELAY_5US
-	rcall DELAY_5US
-	out USICR, r19 ; -
 
-	rcall DELAY_5US
-	rcall DELAY_5US
-	out USICR, r19 ; _
-	rcall DELAY_5US
-	rcall DELAY_5US
-	out USICR, r20 ; 
-	rcall DELAY_5US
-	rcall DELAY_5US
-	out USICR, r19 ; -
+; TODO debugar os pulsos de clock
+   ; SEND DATA START
 
-	rcall DELAY_5US
-	rcall DELAY_5US
-	out USICR, r19 ; _
-	rcall DELAY_5US
-	rcall DELAY_5US
-	out USICR, r20 ; 
-	rcall DELAY_5US
-	rcall DELAY_5US
-	out USICR, r19 ; -
+	; ldi r22, 0b11101111
+	; out USIDR, r22
 
-	rcall DELAY_5US
-	rcall DELAY_5US
-	out USICR, r19 ; _
-	rcall DELAY_5US
-	rcall DELAY_5US
-	out USICR, r20 ; 
-	rcall DELAY_5US
-	rcall DELAY_5US
-	out USICR, r19 ;LSB - 
 
-	rcall DELAY_5US
-	rcall DELAY_5US
-	out USICR, r19 ; _
-	rcall DELAY_5US
-	rcall DELAY_5US
+
+
+	; out USICR, r19 ; -
+	; rcall DELAY_5US
+	; rcall DELAY_5US
+	; out USICR, r19 ; _
+	; rcall DELAY_5US
+	; rcall DELAY_5US
+	; out USICR, r20 ; Shift
+	; rcall DELAY_5US
+	; rcall DELAY_5US 
+
+	; out USICR, r19 ; -
+	; rcall DELAY_5US
+	; rcall DELAY_5US
+	; out USICR, r19 ; _
+	; rcall DELAY_5US
+	; rcall DELAY_5US
+	; out USICR, r20 ; Shift
+	; rcall DELAY_5US
+	; rcall DELAY_5US 
+
+	; out USICR, r19 ; -
+	; rcall DELAY_5US
+	; rcall DELAY_5US
+	; out USICR, r19 ; _
+	; rcall DELAY_5US
+	; rcall DELAY_5US
+	; out USICR, r20 ; Shift
+	; rcall DELAY_5US
+	; rcall DELAY_5US 
+
+	; out USICR, r19 ; -
+	; rcall DELAY_5US
+	; rcall DELAY_5US
+	; out USICR, r19 ; _
+	; rcall DELAY_5US
+	; rcall DELAY_5US
+	; out USICR, r20 ; Shift
+	; rcall DELAY_5US
+	; rcall DELAY_5US 
+
+	; out USICR, r19 ; -
+	; rcall DELAY_5US
+	; rcall DELAY_5US
+	; out USICR, r19 ; _
+	; rcall DELAY_5US
+	; rcall DELAY_5US
+	; out USICR, r20 ; Shift
+	; rcall DELAY_5US
+	; rcall DELAY_5US 
+
+	; out USICR, r19 ; -
+	; rcall DELAY_5US
+	; rcall DELAY_5US
+	; out USICR, r19 ; _
+	; rcall DELAY_5US
+	; rcall DELAY_5US
+	; out USICR, r20 ; Shift
+	; rcall DELAY_5US
+	; rcall DELAY_5US 
+
+	; out USICR, r19 ; -
+	; rcall DELAY_5US
+	; rcall DELAY_5US
+	; out USICR, r19 ; _
+	; rcall DELAY_5US
+	; rcall DELAY_5US
+	; out USICR, r20 ; Shift
+	; rcall DELAY_5US
+	; rcall DELAY_5US 
+
+	; out USICR, r19 ; -
+	; rcall DELAY_5US
+	; rcall DELAY_5US
+	; out USICR, r19 ; _
+	; rcall DELAY_5US
+	; rcall DELAY_5US
+	; out USICR, r20 ; Shift
+	; rcall DELAY_5US
+	; rcall DELAY_5US 
+
+	; ;CLear overflow flag
+	; ldi r21, (1<<USIOIF)
+	; out USISR, r21 ;
+
+	; out USICR, r19 ; -
+
+
+	; ACK - Como o byte passado é lido? Usar a estratégia do tutorial para da o pulso e limpar o USIOIF
+	; rcall DELAY_5US
+	; rcall DELAY_5US
+	; out USICR, r19 ; _
+
+	; ; set sda como input
+	; in r21, DDRB
+	; cbr r21, PINB5
+	; out DDRB, r21
+
+	; ;puso no scl
+
+	; rcall DELAY_5US
+	; rcall DELAY_5US
+
+	; out USICR, r19 ; -
+
+	; ; rcall DELAY_5US
+	; ; rcall DELAY_5US
+
+	; ; out USICR, r19 ; _
+	; ; ; ACK END 	
+	
+
+	; ; set sda como output
+	; in r21, DDRB
+	; sbr r21, PINB5
+	; out DDRB, r21
+
+	; ldi r21, 0
+	; out USIDR, r21
+
+	; ;stop condition
+
+	; out USICR, r19 ; -
+
+	; rcall DELAY_5US
+	; rcall DELAY_5US
+
+	; ldi r21, 0xFF
+	; out USIDR, r21
+
+	; ;stop condition
+
+	; rcall DELAY_5US
+	; rcall DELAY_5US
+
+	; ;CLear stop flag
+	; ldi r21, (1<<USIPF)
+	; out USISR, r21 ;
 
 	ldi r19, 0
 	out USICR, r19
 
-	sbi PORTB, PINB7
 	sbi PORTB, PINB5
+	sbi PORTB, PINB7
+	;end
 
-	
-	;TODO ACK
 
-	ret
+; TODO
+
+ret
